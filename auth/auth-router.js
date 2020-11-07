72aa01a754f4e6b8c345561a3e037f8e7d7ebac1
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-11-07 15:26:32 -0700

    working on auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,61 @@
-const router = require('express').Router();
+const router = require("express").Router()
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("./users-model")
 
-router.post('/register', (req, res) => {
+router.post('/register', (req, res, next) => {
   // implement registration
+  try {
+      const { username, password } = req.body
+      const user = await Users.findByUsername(username)
+        if (user) {
+          return res.status(409).json({
+            message: `The username "${user.username}" is already taken`
+          })
+        }
+      const newUser = await Users.add({
+        username,
+        password: await bcrypt.hash(password, 14)
+      })
+
+      res.status(201).json(newUser)
+  } catch(err) {
+    next(err)
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   // implement login
+  try {
+    const { username, password } = req.body
+    const user = await Users.findByUsername(username)
+
+    if(!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const token = jwt.sign({
+      userID: user.id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.json({
+      message: `Hello again! We missed you, ${user.username}!`
+    })
+  } catch(err) {
+    next(err)
+  }
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,35 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const jwt = require("jsonwebtoken")
+
+function authenticate() { async (req, res, next) => {
+  try {
+    const token = req.cookies.token
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Invalid credentials"
+        })
+      }
+
+      req.token = decoded
+
+        next()
+      })
+    } catch(err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = {
+  authenticate,
+}
